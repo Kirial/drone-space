5c06cba990f523b8460ce94630b40fff8c2b044c
Triblex <msc95x@gmail.com>
2017-06-05 12:40:19 +0200

    FindCountourVideoFeed

---
+++ Alpha-Learning/FindCountourVideoFeed/src/ofApp.cpp
@@ -0,0 +1,100 @@
+#include "ofApp.h"
+
+void ofApp::setup() {
+
+	bLearnBackground = false;
+
+	vidGrabber.setVerbose(true);
+	vidGrabber.initGrabber(320, 240);
+
+	colorImg.allocate(320, 240);
+	grayImage.allocate(320, 240);
+	grayBg.allocate(320, 240);
+	grayDiff.allocate(320, 240);
+}
+
+void ofApp::update() {
+	vidGrabber.update();
+	//do we have a new frame?
+	if (vidGrabber.isFrameNew()) {
+		colorImg.setFromPixels(vidGrabber.getPixels());
+		grayImage = colorImg; // convert our color image to a grayscale image
+		if (bLearnBackground == true) {
+			grayBg = grayImage; // update the background image
+			bLearnBackground = false;
+		}
+		grayDiff.absDiff(grayBg, grayImage);
+		grayDiff.threshold(30);
+		contourFinder.findContours(grayDiff, 5, (340 * 240) / 4, 4, false, true);
+	}
+}
+
+void ofApp::draw() {
+	ofSetHexColor(0xffffff);
+	colorImg.draw(0, 0, 320, 240);
+	grayDiff.draw(0, 240, 320, 240);
+	ofDrawRectangle(320, 0, 320, 240);
+	contourFinder.draw(320, 0, 320, 240);
+	ofColor c(255, 255, 255);
+	for (int i = 0; i < contourFinder.nBlobs; i++) {
+		ofRectangle r = contourFinder.blobs.at(i).boundingRect;
+		r.x += 320; r.y += 240;
+		c.setHsb(i * 64, 255, 255);
+		ofSetColor(c);
+		ofDrawRectangle(r);
+	}
+}
+
+void ofApp::keyPressed(int key) {
+	bLearnBackground = true;
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
+
+}
